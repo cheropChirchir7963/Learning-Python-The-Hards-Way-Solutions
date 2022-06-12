35a723b6786e5c410fc5988cc8cd6b67a607da1f
Mercy Wacera <waceraaamercy@gmail.com>
2022-06-12 15:58:27 +0300

    completed my solution to exercise 22

---
+++ Exercise 22/exer22.py
@@ -0,0 +1,29 @@
+# What Do You Know So Far?
+
+name = "Mercy Wacera"
+age = 23
+height = 34.5
+weight = 23
+School = "AkiraChix"
+Year = 2022
+
+
+
+total = age + height + weight 
+print(f"If I add {age}, {height}, and {weight} I get {total}.") 
+print(" ")
+
+x = "{} {} {} {} {} {} {} {} {}" 
+
+print(x.format(1,3,5,6,3,8,9,3,9)) 
+print(x.format("one", "five", "eight", "twenty","fifteen", "twelve", "thirty", "fourty four", "one")) 
+print(x.format(False, False, True, True, False, False, True, False, True)) 
+
+char = "a e i o u" 
+alpherbet = "a\nb\nc\nd\ne\nf\ng\nh\ni" 
+ 
+print("Here are the vowels: ", char) 
+print("Here are the alpherbetical: ", alpherbet) 
+
+students = "\t*Mary Wairimu\n\t*Meghan Andrews\n\t*Collin Mwangi"
+print(students)
